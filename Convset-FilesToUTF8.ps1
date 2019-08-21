4117aa2d57a84ed145e0795b269eb4b34991ead8
korniienkoalexis <korniienkoalexis@gmail.com>
2019-08-21 15:34:39 -0400

    Add files via upload

--- Convset-FilesToUTF8.ps1
+++ Convset-FilesToUTF8.ps1
@@ -1,10 +1,16 @@
-﻿$folderNameForConvertedFiles = "ConvertedFiles" #Specifie folder name for converted files
+﻿<# 
+. [SCRIPT/CMDLET NAME]
+. Convset-FilesToUTF8.PS1
+. [EXAMPLE]
+. Convset-FilesToUTF8.PS1 -Encoding "C:\folderWithSqlFiles"
+. Convset-FilesToUTF8.PS1 -Convert "C:\folderWithSqlFiles"
+#>
 
-Function Convert-FilesToUTF8 {
+Param([switch]$Encoding,[parameter (Mandatory=$true)][string]$path,[switch]$Convert)
 
-Param (
-[parameter (Mandatory=$true)][string]$path
-)
+############################################## Adjust these variables#############################################
+$folderNameForConvertedFiles = "ConvertedFiles" #Specifie folder name for converted files
+##################################################################################################################
 
 function Get-Encoding {
 param([Parameter(ValueFromPipeline=$True)] $filename)      
@@ -22,40 +28,70 @@ process {
 }
 
 
-Get-ChildItem $path -File *.sql | foreach { $file = $_.FullName
+Function Convert-FilesToUTF8 ($path) {
 
- 
- $file = "'" + $file + "'"
- $code = 'Get-Encoding' +" " + $file
- Invoke-Expression $code | where {$_.BodyName -ne "utf-8"} | foreach {
+#Param (
+#[parameter (Mandatory=$true)][string]$path
+#)
 
-    $fileToConvert = $_.FullPath #Full path to original file for converting
+    Get-ChildItem $path -File *.sql | foreach { 
+    $file = $_.FullName 
+    $file = "'" + $file + "'"
+    $code = 'Get-Encoding' +" " + $file
+    Invoke-Expression $code | where {$_.BodyName -ne "utf-8"} | foreach {
 
-    #$fileAfterConverting = $fileToConvert.Insert(($fileToConvert.LastIndexOf('.')),"_UTF8") #path to the file after converting, this line add _UTF8 to the end in file name
+        $fileToConvert = $_.FullPath #Full path to original file for converting
+
+        #$fileAfterConverting = $fileToConvert.Insert(($fileToConvert.LastIndexOf('.')),"_UTF8") #path to the file after converting, this line add _UTF8 to the end in file name
 
     #####################Creating Folder for converted files (Comment next 5 lines if using filename with _UTF8 in the end)##############################################################################################
     $fileAfterConverting = $path + "\" + $folderNameForConvertedFiles + "\" +$_.Name #Generate a path for saving converted files in specified folser
-    if (-not (Test-Path -LiteralPath $path)) 
+    if (-not (Test-Path -LiteralPath ($path+"\"+ $folderNameForConvertedFiles))) 
         {
             New-Item -Path $path -Name $folderNameForConvertedFiles -ItemType "directory" #Check for existing and create folder for converted files
         }
    ########################################################################################################################################################
    
-    try 
-        {
-        Get-Content -Path $fileToConvert | Out-File -FilePath $fileAfterConverting -Encoding UTF8 -ErrorAction Stop #convertig File and saving to path from variable $fileAfterConverting
+
+        Get-Content -Path $fileToConvert | Out-File -FilePath $fileAfterConverting -Encoding UTF8 #convertig File and saving to path from variable $fileAfterConverting
         Write-Host "File " $fileAfterConverting "converted" -ForegroundColor Green
-        }
-            Catch
-            {
-            $_.Exception
-            }
+
     }
-}
+    }
 
 }
 
-Convert-FilesToUTF8
 
-    #$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
-    #[System.IO.File]::WriteAllLines($fileAfterConverting, (Get-Content -Path $fileToConvert), $Utf8NoBomEncoding)
+if($Encoding)
+{
+        Get-ChildItem $path -File *.sql | foreach { 
+        $file = $_.FullName
+        $file = "'" + $file + "'"
+        $code = 'Get-Encoding' +" " + $file
+        Invoke-Expression $code
+        }
+
+}
+
+if ($Convert)
+{
+    Convert-FilesToUTF8 $path
+    Write-Host "Checking files in folder" ($path+"\"+ $folderNameForConvertedFiles) -ForegroundColor DarkYellow
+    Get-ChildItem ($path+"\"+ $folderNameForConvertedFiles) -File *.sql | foreach { $file = $_.FullName
+
+    $file = "'" + $file + "'"
+    $code = 'Get-Encoding' +" " + $file
+    Invoke-Expression $code
+        }
+
+}
+
+if (-not($Convert.IsPresent) -and -not($Encoding.IsPresent))
+{
+        Get-ChildItem $path -File *.sql | foreach { 
+        $file = $_.FullName
+        $file = "'" + $file + "'"
+        $code = 'Get-Encoding' +" " + $file
+        Invoke-Expression $code
+        }
+}
